5bc1e087dee12c6debd6673f473e9b6ffb59e534
AK <amashokkumar@hotmail.com>
2021-05-17 15:38:30 +0530

    server side testing

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
 import React from 'react';
 
 class Movie extends React.Component {
     componentDidMount() {
         const apiUrl = 'http://3.108.14.110/api/v1/movie/list';
-        fetch(apiUrl)
+        axios.get(apiUrl)
             .then((response) => response.json())
             .then((data) => console.log('This is your data', data));
     }
